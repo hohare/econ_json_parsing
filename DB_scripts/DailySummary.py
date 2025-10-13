5144accdc765dad662b75c90430ee8dee7e39249
Danny Noonan <daniel.christopher.noonan@cern.ch>
2025-10-13 16:44:51 -0500

    update summary scripts to handle empty csv files from weeks we didn't test and T/D chips

--- DB_scripts/DailySummary.py
+++ DB_scripts/DailySummary.py
@@ -12,29 +12,54 @@ f_D = glob.glob(f'{csv_path}/econdCSV/econd_chip_test_results_{datetime.datetime
 f_T = glob.glob(f'{csv_path}/econtCSV/econt_chip_test_results_{datetime.datetime.now().year}-{datetime.datetime.now().month:02d}-{datetime.datetime.now().day:02d}_*.csv')[0]
 
 
-df_D = pd.read_csv(f_D,index_col=0).sort_index()
-df_T = pd.read_csv(f_T,index_col=0).sort_index()
-df_D.Timestamp = pd.to_datetime(df_D.Timestamp)
-df_T.Timestamp = pd.to_datetime(df_T.Timestamp)
-df_D['Pass'] = df_D['Pass/Fail']=='Pass'
-df_D['Fail'] = df_D['Pass/Fail']=='Fail'
-df_T['Pass'] = df_T['Pass/Fail']=='Pass'
-df_T['Fail'] = df_T['Pass/Fail']=='Fail'
-by_week_D=df_D.groupby(pd.Grouper(key='Timestamp', freq='3h'))
-by_week_T=df_T.groupby(pd.Grouper(key='Timestamp', freq='3h'))
+try:
+    df_D = pd.read_csv(f_D,index_col=0).sort_index()
+    df_D.Timestamp = pd.to_datetime(df_D.Timestamp)
+    df_D['Pass'] = df_D['Pass/Fail']=='Pass'
+    df_D['Fail'] = df_D['Pass/Fail']=='Fail'
+    by_week_D=df_D.groupby(pd.Grouper(key='Timestamp', freq='3h'))
 
+    summary_D = by_week_D.count()[['Pass/Fail']]
+    summary_D.columns = ['Total']
+    summary_D['Passing'] = by_week_D.sum()['Pass']
+    summary_D['Failing'] = by_week_D.sum()['Fail']
+except:
+    summary_D = None
 
-summary_D = by_week_D.count()[['Pass/Fail']]
-summary_D.columns = ['Total']
-summary_D['Passing'] = by_week_D.sum()['Pass']
-summary_D['Failing'] = by_week_D.sum()['Fail']
+try:
+    df_T = pd.read_csv(f_T,index_col=0).sort_index()
+    df_T.Timestamp = pd.to_datetime(df_T.Timestamp)
+    df_T['Pass'] = df_T['Pass/Fail']=='Pass'
+    df_T['Fail'] = df_T['Pass/Fail']=='Fail'
+    by_week_T=df_T.groupby(pd.Grouper(key='Timestamp', freq='3h'))
 
-summary_T = by_week_T.count()[['Pass/Fail']]
-summary_T.columns = ['Total']
-summary_T['Passing'] = by_week_T.sum()['Pass']
-summary_T['Failing'] = by_week_T.sum()['Fail']
+    summary_T = by_week_T.count()[['Pass/Fail']]
+    summary_T.columns = ['Total']
+    summary_T['Passing'] = by_week_T.sum()['Pass']
+    summary_T['Failing'] = by_week_T.sum()['Fail']
+except:
+    summary_T = None
 
-summary = summary_D.merge(summary_T,how='outer',left_index=True,right_index=True,suffixes=('_ECOND','_ECONT')).fillna(0).astype(int)
+if summary_T is None and summary_D is None:
+    print('NO DATA')
+    client = WebClient(token=token)
+
+    result = client.chat_postMessage(channel = "cms-econ-asic",
+                                     text = 'No chip test results found from past week.',
+                                     username = "Bot User")
+
+elif summary_T is None:
+    print('NO DATA for ECONT, copying structure from ECOND')
+    summary_T = summary_D.copy(deep=True)
+    summary_T.loc[:,:] = 0
+    summary = summary_D.merge(summary_T,how='outer',left_index=True,right_index=True,suffixes=('_ECOND','_ECONT')).fillna(0).astype(int)
+elif summary_D is None:
+    print('NO DATA for ECOND, copying structure from ECONT')
+    summary_D = summary_T.copy(deep=True)
+    summary_D.loc[:,:] = 0
+    summary = summary_D.merge(summary_T,how='outer',left_index=True,right_index=True,suffixes=('_ECOND','_ECONT')).fillna(0).astype(int)
+else:
+    summary = summary_D.merge(summary_T,how='outer',left_index=True,right_index=True,suffixes=('_ECOND','_ECONT')).fillna(0).astype(int)
 
 today = datetime.datetime.now()
 yesterday=(today-datetime.timedelta(days=1))
